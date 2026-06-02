dbbd1ae2a67b6906e3d98687d9d52a0f3eb5043e
Alejandro <alejandro.sosa@tecnoaccion.com.ar>
2026-06-02 12:08:29 -0300

    Trabajo practico

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        ClientePersonaFisica cliente1 = new ClientePersonaFisica(
+                1, "Juan", "Perez", "DNI 30123456",
+                "Av. Corrientes 1234", "011-4444-5555", "juan.perez@gmail.com", "15/03/2020",
+                "30123456", "22/07/1985", "Contador", 150000
+        );
+
+        ClienteEmpresa cliente2 = new ClienteEmpresa(
+                2, "TechSolutions", "S.A.", "CUIT 30-71234567-9",
+                "Av. del Libertador 5000", "011-5555-7777", "info@techsolutions.com", "01/06/2019",
+                "TechSolutions S.A.", "30-71234567-9", "Tecnologia", "Maria Lopez"
+        );
+
+        ClientePremium cliente3 = new ClientePremium(
+                3, "Carlos", "Gomez", "DNI 25987654",
+                "Av. Santa Fe 800", "011-6666-8888", "carlos.gomez@gmail.com", "10/01/2018",
+                5000000, "Laura Martinez", "Tarjeta Black, Sala VIP, Descuento en seguros"
+        );
+
+        cliente1.mostrarDatos();
+        System.out.println();
+        cliente2.mostrarDatos();
+        System.out.println();
+        cliente3.mostrarDatos();
     }
 }
